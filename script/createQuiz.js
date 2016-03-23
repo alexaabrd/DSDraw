c525639440d3fef713f92899a2db476745aec922
Alexa Bird <abird1@trinity.edu>
2016-03-23 18:39:17 -0500

    scripts + makeQuiz

---
+++ script/createQuiz.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+function fillForm() {
+var e = document.getElementById("numQuestions");
+console.log('here');
+console.log(e);
+//var num =  = e.options[e.selectedIndex].value;
+
+var e = document.getElementById("questions");
+var text =    "<input type='textArea' name='Question' > <br> CANVAS goes here";
+
+var text = num;
+e.innerHTML(text);
+
+}
+
+
+
+
+
+
+
+
+
